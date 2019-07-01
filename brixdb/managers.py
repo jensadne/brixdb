20302be8c2cc37f3bbc7284f78f0fe612301580c
Jens Ådne J. Rydland <jensadne@pvv.ntnu.no>
2019-07-01 18:40:56 +0200

    new queryset methods:
    - ElementQuerySet.by_part() for elements of that part
    - updated owned_by_user() / not_owned_by_user() methods
    - method for aggregating counts of a Part owned by User

--- brixdb/managers.py
+++ brixdb/managers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.apps import apps
+from django.db import connection
+from django.db.models.expressions import RawSQL
 from django.db.models.query import QuerySet
 
 from polymorphic.managers import PolymorphicQuerySet
@@ -14,7 +15,20 @@ class SetQuerySet(CatalogItemQuerySet):
 
 
 class PartQuerySet(CatalogItemQuerySet):
-    pass
+    def get_owned_colour_counts(self, part, owner):
+        """
+        Aggregates the number of colours the given User owns the given Part in.
+        """
+        sql = """SELECT c.slug, c.name, SUM(ii.quantity*oi.quantity)
+                FROM brixdb_iteminventory ii, brixdb_colour c, brixdb_element e, brixdb_owneditem oi
+                WHERE ii.element_id = e.id and e.colour_id = c.id AND e.part_id = {part_id}
+                    AND ii.part_of_id = oi.item_id AND oi.owner_id = {owner_id}
+                    GROUP BY c.slug, c.name
+                    ORDER BY SUM DESC
+                 """.format(part_id=part.pk, owner_id=owner.pk)
+        c = connection.cursor()
+        c.execute(sql)
+        return [{'colour_slug': slug, 'colour_name': name, 'count': count} for slug, name, count in c.fetchall()]
 
 
 class ElementQuerySet(QuerySet):
@@ -22,13 +36,15 @@ class ElementQuerySet(QuerySet):
         """
         Returns the Elements `user` doesn't own.
         """
-        ItemElement = apps.get_model('brixdb', 'ItemElement')
-        ielems = ItemElement.objects.filter(item__in=user.sets_owned.values_list('owned_set', flat=True))
+        from .models import ItemInventory
+        # TODO: use recursive query for owned Items
+        ielems = ItemInventory.objects.filter(part_of__in=user.sets_owned.values_list('owned_set', flat=True))
         return self.exclude(pk__in=ielems.values_list('element', flat=True)).distinct()
 
     def owned_by(self, user):
-        ItemElement = apps.get_model('brixdb', 'ItemElement')
-        ielems = ItemElement.objects.filter(item__in=user.sets_owned.values_list('owned_set', flat=True))
+        from .models import ItemInventory
+        # TODO: use recursive query for owned Items
+        ielems = ItemInventory.objects.filter(part_of__in=user.owned_items.values_list('item', flat=True))
         return self.filter(pk__in=ielems.values_list('element', flat=True)).distinct()
 
     def for_user(self, user, owned=True):
@@ -36,6 +52,9 @@ class ElementQuerySet(QuerySet):
 
     def by_colour(self, colour):
         return self.filter(colour=colour)
+
+    def by_part(self, part):
+        return self.filter(part=part)
 
 
 class MinifigQuerySet(CatalogItemQuerySet):
